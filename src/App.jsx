e8f4a1ab5a5b486199dabea0f12f6bf7245fcebf
Charbel <charbelghosn24@hotmail.com>
2024-07-12 15:41:36 -0700

    puppy info added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [puppies,setPuppies]=useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
-    <>
-      <div>
+    <div className="app">
+      <div className="puppylist">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
         })}
       </div>
-    </>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
