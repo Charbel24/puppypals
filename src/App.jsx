9787a5a4bf8ce94176ed9f3251295dc82e8337d2
Charbel <charbelghosn24@hotmail.com>
2024-07-12 14:49:29 -0700

    puppy data with use state is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-
-import './App.css'
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
+  const [puppies,setPuppies]=useState(puppyList)
 
   return (
     <>
-     
+      <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
